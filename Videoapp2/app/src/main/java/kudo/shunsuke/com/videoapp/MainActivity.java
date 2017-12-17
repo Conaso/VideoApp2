7799815ffe0622898d8bb9942d95bfef0348431b
hunsuke_KUDOH <910shunsuke@gmail.com>
2017-12-17 19:02:00 +0900

    2017/12/17

--- Videoapp2/app/src/main/java/kudo/shunsuke/com/videoapp/MainActivity.java
+++ Videoapp2/app/src/main/java/kudo/shunsuke/com/videoapp/MainActivity.java
@@ -4,20 +4,36 @@ package kudo.shunsuke.com.videoapp;
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.Intent;
+import android.graphics.Path;
 import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
+
+import com.coremedia.iso.boxes.Container;
+import com.googlecode.mp4parser.authoring.Movie;
+import com.googlecode.mp4parser.authoring.Track;
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
+import com.googlecode.mp4parser.authoring.tracks.AppendTrack;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int RESULT_PICK_VIDEOFILE = 1001;
-    private TextView dcimPath;
     private VideoView videoView;
+
+
 
 
     @Override
@@ -47,22 +63,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+
+        // ほりー参上
 //intentで複数の動画を持ってきた、かつ、それが成功した時、
-        if (requestCode == RESULT_PICK_VIDEOFILE && resultCode == Activity.RESULT_OK){
+        if (requestCode == RESULT_PICK_VIDEOFILE && resultCode == Activity.RESULT_OK) {
             //何もなくする。
             Uri uri = null;
 
             //データが何かあった時、。
-            if (data != null){
+            if (data != null) {
 
 
                 //データの中から選択したデータを留める。
                 ClipData clip = data.getClipData();
 
-                if (clip  == null) {
+                if (clip == null) {
                     Toast.makeText(getApplicationContext(), "Clip is nothing", Toast.LENGTH_SHORT).show();
                     return;
                 }
@@ -71,11 +89,53 @@ public class MainActivity extends AppCompatActivity {
                 for (int i = 0; i < clip.getItemCount(); i++) {
                     contentUri[i] = clip.getItemAt(i).getUri();
                 }
+                //URIというファイルで再生している
+                videoView.setVideoURI(contentUri[0]);
+                Uri uri1 = contentUri[0];
+                String path = uri1.getPath();
+                Uri uri2 = contentUri[1];
+                String path1 = uri2.getPath();
 
-                    //videoviewに写す
-                    uri = data.getData();
-                    videoView.setVideoURI(uri);
+                try {
+                    Movie[] inMovies = new Movie[]{
+                            MovieCreator.build(path),
+                            MovieCreator.build(path1)};
+
+                    List<Track> videoTracks = new LinkedList<Track>();
+                    List<Track> audioTracks = new LinkedList<Track>();
+                    for (Movie m : inMovies) {
+                        for (Track t : m.getTracks()) {
+                            if (t.getHandler().equals("soun")) {
+                                audioTracks.add(t);
+                            }
+                            if (t.getHandler().equals("vide")) {
+                                videoTracks.add(t);
+                            }
+                        }
+                    }
+                    Movie result = new Movie();
+                    if (audioTracks.size() > 0) {
+                        result.addTrack(new AppendTrack(audioTracks.toArray(new Track[audioTracks.size()])));
+                    }
+                    if (videoTracks.size() > 0) {
+                        result.addTrack(new AppendTrack(videoTracks.toArray(new Track[videoTracks.size()])));
+                    }
+
+
+                    Container out = new DefaultMp4Builder().build(result);
+                    String outputFilePath = Environment.getExternalStorageDirectory() + "/output_append.mp4";
+                    FileOutputStream fos = new FileOutputStream(new File(outputFilePath));
+                    out.writeContainer(fos.getChannel());
+                    fos.close();
+
+                        }catch(Exception e){
+
+                        }
+                    }
+                }
             }
-        }
-    }
+
 }
+
+
+
